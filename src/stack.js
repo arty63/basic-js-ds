055c49e4a4f2727f11e52dae3e0c00a3cd91f6d4
Artem <artjom63@mail.ru>
2025-11-10 22:16:06 +0300

    feat: add stack

--- src/stack.js
+++ src/stack.js
@@ -12,23 +12,23 @@ const { NotImplementedError } = require('../lib/errors');
  * stack.pop(); // undefined
  *
  */
-class Stack {
-  push(/* value */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+ class Stack {
+  constructor(){
+    this.stack = []
+  }
+  
+  push(element) {
+    this.stack.push(element)
   }
 
   pop() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return this.stack.pop()
   }
 
   peek() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return this.stack[this.stack.length-1]  
   }
 }
-
 module.exports = {
   Stack,
 };
